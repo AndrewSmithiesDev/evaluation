a60aaceb04e132adf5de9d47188a2916b31d221a
Andrew Smithies <andrewsmithies.dev@gmail.com>
2026-02-06 09:27:57 +0100

    Phase 2 : Versioning et Configuration

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $now = new \DateTime();
+
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'now' => $now,
+        ]);
+    }
+}
